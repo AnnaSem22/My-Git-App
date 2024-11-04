ca0b3fa325d839cafee13b714a2cd4cb57ea723a
Anna <plombamexx@gmail.com>
2024-11-04 19:03:33 +0300

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,19 @@
 package app;
-
+/*
 public class Main {
     public static void main(String[] args) {
         System.out.println("My git app.");
+    }
+}*/
+/*public class Main {
+    public static void main(String[] args) {
+        System.out.println("My git app for Users.");
+        System.out.println("The informative application.");
+    }
+}*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My git app for Users.");
+        System.out.println("The informative application.");
     }
 }
